c29dd841bdecc74ebb42ceae7ea0da5b10014b37
BuildTools <unconfigured@null.spigotmc.org>
2022-04-18 23:59:18 +0500

    Added polyakov_parsing.py

---
+++ polyakov_parsing.py
@@ -0,0 +1,41 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def random_task(task) -> dict:
+    """
+    Парсит задание по ссылке типа
+    https://kpolyakov.spb.ru/school/ege/gen.php?action=viewVar&select={task}&answers=on&varId=
+    Возвращает словарь
+    {"html": html_content, "id": task_id, "answer": task_answer}
+    :param task: Номер задания в КИМ от 1 до 27
+    :return: dict
+    """
+    url = f"https://kpolyakov.spb.ru/school/ege/gen.php?action=viewVar&select={task}&answers=on&varId="
+    response = requests.get(url)
+    if response:
+        soup = BeautifulSoup(response.text, "lxml")
+        tag_td = soup.find("td", {"class": "topicview"})
+        script_lines = tag_td.find("script").text.strip().split("\n")
+        task_id = re.search(r"\d+", script_lines[0]).group(0)
+        html_content = re.findall(r"""\(([^\[\]]+)\)""", script_lines[1].replace("(", "", 1).strip()[:-2])
+
+        tag_table = soup.find("table", {"class": "varanswer"})
+        answer_table = {}
+        for i, row in enumerate(tag_table.findAll("tr")):
+            if i == 0:
+                continue
+            for egeno, answer in zip(row.find_all("td", {"class": "egeno"}), row.find_all("td", {"class": "answer"})):
+                answer_table[egeno.text[:-1]] = answer.text
+        return {"html": html_content, "id": task_id, "answer": answer_table[str(task)]}
+
+    raise Exception("Невозможно обратиться к сайту Полякова")
+
+
+if __name__ == "__main__":
+    # пример
+    a = random_task(1)
+    print(a["id"])
+    print(a["answer"])
+    print(a["html"])
