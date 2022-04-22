21ab99effbc414b64ada60410161d1a17484f604
Dinislam <dinislamsayfullin@gmail.com>
2022-04-22 20:28:58 +0500

    update case page

--- data/tasks.py
+++ data/tasks.py
@@ -5,6 +5,6 @@ from .db_session import SqlAlchemyBase
 class Task(SqlAlchemyBase):
     __tablename__ = 'tasks'
 
-    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     html = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     answer = sqlalchemy.Column(sqlalchemy.String, nullable=False)
