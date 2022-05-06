aa449e6eee82ce92c1fdd46c0662022cc38bcd04
Just1nWTF <just1ncs@yandex.ru>
2022-05-06 19:21:51 +0500

    Add number validator

--- forms/task.py
+++ forms/task.py
@@ -1,10 +1,16 @@
+from re import fullmatch
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FileField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+
+def number_check(form, field):
+    if not fullmatch(r'^([1-9]|[1][\d]|2[2-7])$', field.data):
+        raise ValidationError("Номер задачи должен быть числом от 1 до 27")
 
 
 class TaskForm(FlaskForm):
-    number = StringField('Номер задачи', validators=[DataRequired()])
+    number = StringField('Номер задачи', validators=[DataRequired(), number_check])
     task = StringField('Условие задачи', validators=[DataRequired()])
     answer = StringField('Ответ', validators=[DataRequired()])
     file1 = FileField('Файл 1')
